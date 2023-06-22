dcbdf25c1c9301a34a2f791585fa4761317dad6b
Victor Hugo <victorkyhugo@gmail.com>
2023-06-22 13:45:18 -0300

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout>
+    <q-header elevated>
+      <q-toolbar class="m-2 bg-amber-700">
+        <q-toolbar-title> Orchestra Gifs </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script></script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex-center flex"></q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
