00ef54608625fa0c27f7b343beaf87ded5905b79
victorkyhugo <victorkyhugo@gmail.com>
2023-06-23 10:57:36 -0300

    style: adjust open gif size

--- src/components/GifModalComponent.vue
+++ src/components/GifModalComponent.vue
@@ -3,10 +3,11 @@
     <div
       @click="toggleModal"
       data-test="modal-background"
-      class="absolute bottom-0 top-0 z-20 min-h-full w-20 min-w-full bg-black opacity-60"
+      class="absolute bottom-0 top-0 z-20 min-h-full min-w-full bg-black opacity-60"
     ></div>
     <q-img
-      class="fixed inset-0 z-30 mx-auto my-auto aspect-square w-4/12"
+      class="fixed inset-0 z-30 mx-auto my-auto aspect-video h-2/3 w-1/2"
+      spinner-color="white"
       :src="getSelectedGif"
     />
     <q-icon
@@ -14,7 +15,7 @@
       data-test="modal-close"
       size="3rem"
       @click="toggleModal"
-      class="fixed right-6 top-20 z-40 cursor-pointer"
+      class="fixed right-6 top-20 z-40 cursor-pointer text-gray-100"
     />
   </div>
 </template>
